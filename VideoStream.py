b93108c62167f3aac6e8038921b65a3ea8738b10
MiBoza <miguel.b31415@gmail.com>
2020-06-14 22:17:49 -0600

    Quality of life

--- VideoStream.py
+++ VideoStream.py
@@ -23,7 +23,7 @@ while(True):
     # cv2.imwrite(name, frame)
 
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
