bf155ef00b0327671c5efc6e51aec21da26816ca
MiBoza <miguel.b31415@gmail.com>
2020-06-15 19:18:45 -0600

    Guarda imágenes

--- VideoStream.py
+++ VideoStream.py
@@ -1,14 +1,15 @@
 # For more info: http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_video_display/py_video_display.html
 import cv2
 import numpy as np
+import os
 
 # Playing video from file:
 # cap = cv2.VideoCapture('vtest.avi')
 # Capturing video from webcam:
 cap = cv2.VideoCapture(0)
+f = '0'
 
-currentFrame = 0
-while(True):
+for currentFrame in range(1, 49):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
@@ -16,23 +17,22 @@ while(True):
     frame = cv2.flip(frame,1)
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Saves image of the current frame in jpg file
-    # name = 'frame' + str(currentFrame) + '.jpg'
-    # cv2.imwrite(name, frame)
+    if(currentFrame == 10):
+        f = ''
+    name = f + str(currentFrame) + '.jpg'
+    cv2.imwrite(name, frame)
 
     # Display the resulting frame
     cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-    # To stop duplicate images
-    currentFrame += 1
+    cv2.waitKey(42)
 
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
+os.system('mv *.jpg Capture/')    #Move file
 
 # Potential Error:
 # OpenCV: Cannot Use FaceTime HD Kamera
